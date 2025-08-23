a4d0a61d3e49f70399d2ca67536053260df5140b
Mubarak <adebowaleraji0103@gmail.com>
2025-08-23 20:44:48 +0100

    d

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import Student from "./props practice/Studen";
 
 function App() {
   return (
-    <div className="flex flex-col gap-7">
+    <div className="flex flex-col gap-7 py-10">
       <Student name = "Mubarak" age = {30}/>
       <Student name = "Habeeb" age = {20}/>
+      <Student name = "Yusuf" age = {10}/>
+
+      <Student name = "Mubarak"/>
     </div>
   )
 
--- src/props practice/Studen.jsx
+++ src/props practice/Studen.jsx
@@ -1,9 +1,15 @@
 function Student(props) {
   return (
-    <div className="border w-fit py-2 ">
+  <div>
+      <div className="border w-fit py-2 px-5">
       <p>Name: {props.name} </p>
       <p>Age: {props.age}</p>
     </div>
+
+    <div>
+        <p>Hello {props.name} welcome </p>
+    </div>
+  </div>
   );
 }
 export default Student;
