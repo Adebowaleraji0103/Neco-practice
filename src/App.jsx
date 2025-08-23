4167c854d6dc79626f8a16562e8cacaa625ada9c
Mubarak <adebowaleraji0103@gmail.com>
2025-08-23 20:40:23 +0100

    c

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Student from "./props practice/Studen";
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col gap-7">
       <Student name = "Mubarak" age = {30}/>
       <Student name = "Habeeb" age = {20}/>
     </div>
