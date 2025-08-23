96ff207697c736835554e8c86a3e1f6fd1d34ed7
Mubarak <adebowaleraji0103@gmail.com>
2025-08-23 20:51:26 +0100

    d

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Student from "./props practice/Studen";
 
 function App() {
   return (
-    <div className="flex flex-col gap-7 py-10">
+    <div className="flex flex-col gap-3 pt-5">
       {/* <Student name = "Mubarak" age = {30}/>
       <Student name = "Habeeb" age = {20}/>
       <Student name = "Yusuf" age = {10}/> */}
@@ -10,6 +10,13 @@ function App() {
       <Student name = "Mubarak"/>
       <Student name = "Mathew"/>
       <Student name = "Sarah"/>
+
+
+      <Student label = "Click Me"/>
+      <Student label = "Submit"/>
+      <Student label = "Delete"/>
+
+
     </div>
   )
 
--- src/props practice/Studen.jsx
+++ src/props practice/Studen.jsx
@@ -1,15 +1,26 @@
 function Student(props) {
   return (
-  <div>
+    <div>
       {/* <div className="border w-fit py-2 px-5">
       <p>Name: {props.name} </p>
       <p>Age: {props.age}</p>
     </div> */}
 
-    <div>
+      <div>
         <p>Hello {props.name} welcome </p>
+      </div>
+
+<div>
+    <button><a href=""> {props.label}</a></button>
+</div>
+
     </div>
-  </div>
   );
 }
 export default Student;
+
+{
+  /* <Button label="Click Me" />
+<Button label="Submit" />
+<Button label="Delete" /> */
+}
