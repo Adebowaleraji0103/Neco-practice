7772fe8f02a875f8a1cff386029db0e8eb0aa144
Mubarak <adebowaleraji0103@gmail.com>
2025-08-24 10:28:26 +0100

    cd

--- src/component/FirstDropDown.jsx
+++ src/component/FirstDropDown.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 
 const FirstDropDown = () => {
 
-const Dropdown = [setIsOpen, IsOpen] = useState (false)
+const [setIsOpen, IsOpen] = useState (false)
     return (
         <div>
-            <button className="bg-green-900 text-white font-semibold text-[20px] py-3 px-5">Menu</button>
+            <button className="items-center flex justify-center m-auto bg-green-900 text-white font-semibold text-[20px] py-2 px-5">Menu</button>
 
         </div>
       );
