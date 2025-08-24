fc7875443574ce1582af4122971d5a44400abd92
Mubarak <adebowaleraji0103@gmail.com>
2025-08-24 11:18:12 +0100

    d

--- src/component/FirstDropDown.jsx
+++ src/component/FirstDropDown.jsx
@@ -6,11 +6,11 @@ const FirstDropDown = () => {
     <div>
       <button  
       onClick={()=> setIsOpen (!IsOpen)}
-      className="relative items-center flex justify-center m-auto bg-green-900 text-white font-semibold text-[20px] py-2 px-5">
+      className="relative items-center text-center flex justify-center m-auto bg-green-900 text-white font-semibold text-[20px] py-2 px-5">
         Menu
       </button>
       {IsOpen && (
-        <ul className="absolute flex flex-col gap-4 px-[4rem] py-[1rem] justify-center text-center shadow-lg shadow-gray-300">
+        <ul className="absolute flex flex-col gap-4 px-[4rem] py-[1rem] shadow-lg shadow-gray-300">
             <li>Mubby</li>
             <li>Shade</li>
             <li>Ade</li>
