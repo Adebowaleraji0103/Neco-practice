f6011de87e08619dd9df85f3fc1cda70cbca7169
Mubarak <adebowaleraji0103@gmail.com>
2025-08-24 10:53:24 +0100

    jj

--- src/component/FirstDropDown.jsx
+++ src/component/FirstDropDown.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 
 const FirstDropDown = () => {
+  const [setIsOpen, IsOpen] = useState(false);
+  return (
+    <div>
+      <button  
+      onClick={()=> {setIsOpen}}
+      className="items-center flex justify-center m-auto bg-green-900 text-white font-semibold text-[20px] py-2 px-5">
+        Menu
+      </button>
+    </div>
+  );
+};
 
-const [setIsOpen, IsOpen] = useState (false)
-    return (
-        <div>
-            <button className="items-center flex justify-center m-auto bg-green-900 text-white font-semibold text-[20px] py-2 px-5">Menu</button>
-
-        </div>
-      );
-}
- 
 export default FirstDropDown;
