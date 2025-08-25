3c85f7615b3876e3284d90a97c7026cc0389940d
Mubarak <adebowaleraji0103@gmail.com>
2025-08-25 06:48:25 +0100

    f

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,26 +1,30 @@
 import { MdMenu } from "react-icons/md";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 const Navbar = () => {
   return (
-    <header className="flex justify-evenly py-3 px-6 shadow-2xl">
+    <header className="flex justify-between py-3 px-6 shadow-2xl">
       <div>Logo</div>
 
       <div>
-        <MdMenu className="md:hidden"/>
+        <MdMenu className="md:hidden" />
       </div>
 
-        <ul className=" hidden md:flex">
-            <li>Home</li>
-            <li>About Neco</li>
-            <li>Our Exam</li>
-            <li>Neco Result</li>
-            <li>Payment</li>
-            <li>Operators</li>
-            <li>FAQ</li>
-            <li>Support</li>
-        </ul>
+      <ul className=" hidden md:flex gap-8">
+        <li>Home</li>
+        <div className="flex"> 
+          <li>About Neco</li>
+          <RiArrowDropDownLine />
+        </div>
+        <li>Our Exam</li>
+        <li>Neco Result</li>
+        <li>Payment</li>
+        <li>Operators</li>
+        <li>FAQ</li>
+        <li>Support</li>
+      </ul>
 
-        <button>Login</button>
+      <button>Login</button>
     </header>
   );
 };
