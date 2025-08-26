2a71cfdc439cf39838453bb633e61f90fe179111
Mubarak <adebowaleraji0103@gmail.com>
2025-08-26 21:47:32 +0100

    d

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,11 +5,11 @@ import { RiArrowDropDownLine } from "react-icons/ri";
 
 const Navlink = [
   { label: "Home", href: "home" },
-  { label: "About Us", href: "#about" },
-  { label: "Our Exam", href: "exam" },
-  { label: "Payment", href: "payment" },
-  { label: "FAQ", href: "faq" },
-  { label: "Support", href: "support" },
+  // { label: "About Us", href: "#about" },
+  // { label: "Our Exam", href: "exam" },
+  // { label: "Payment", href: "payment" },
+  // { label: "FAQ", href: "faq" },
+  // { label: "Support", href: "support" },
 ];
 
 const Navbar = () => {
