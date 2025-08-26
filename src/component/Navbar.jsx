fb408f0cd530a9005cf37b483c070aff1df392c5
Mubarak <adebowaleraji0103@gmail.com>
2025-08-26 21:14:41 +0100

    f

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -8,6 +8,8 @@ const Navlink = [
   { label: "About Us", href: "#about" },
   { label: "Our Exam", href: "exam" },
   { label: "Payment", href: "payment" },
+  { label: "FAQ", href: "faq" },
+  { label: "Support", href: "support" },
   { label: "Support", href: "support" },
 ];
 
