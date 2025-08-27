6a6da3b7a04e4a044c4ab9086521bb4c323328b2
Mubarak <adebowaleraji0103@gmail.com>
2025-08-27 16:40:22 +0100

    d

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,34 +5,35 @@ import { RiArrowDropDownLine } from "react-icons/ri";
 
 const Navlink = [
   { label: "Home", href: "home" },
-  { label: "About Us", href: "#about" },
-  { label: "Our Exam", href: "exam" },
-  { label: "Payment", href: "payment" },
-  { label: "FAQ", href: "faq" },
-  { label: "Support", href: "support" },
+  // { label: "About Us", href: "#about" },
+  // { label: "Our Exam", href: "exam" },
+  // { label: "Payment", href: "payment" },
+  // { label: "FAQ", href: "faq" },
+  // { label: "Support", href: "support" },
 ];
 
 const Navbar = () => {
   return (
-    <header className="flex justify-between items-center py-2 px-[6rem] font-semibold text-gray-600 shadow-2xl">
-      <div>Logo</div>
-
+    <header className="flex justify-between items-center py-2 px-3 font-semibold text-gray-600 shadow-2xl md:px-[6rem]">
+      <h1>Logo</h1>
+      
       <div>
         <MdMenu className="md:hidden" />
       </div>
 
-      <ul className="flex gap-10">
+      <div className="hidden md:flex">
+        <ul className="gap-10 hidden md:flex">
         {Navlink.map((nav, index) => (
           <li key={index}>
-            <a href={nav.href}>
-            {nav.label}
-
-            </a>
+            <a href={nav.href}>{nav.label}</a>
           </li>
         ))}
       </ul>
 
-      <button className="bg-[#2DC866] text-white py-4 px-5 rounded-[5px]">Login</button>
+      <button className="bg-[#2DC866] text-white py-4 px-5 rounded-[5px]">
+        Login
+      </button>
+      </div>
     </header>
   );
 };
