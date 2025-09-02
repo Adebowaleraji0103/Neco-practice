4a6c2e78efb37906828fc7afa4e1e84cae5d4d33
Mubarak <adebowaleraji0103@gmail.com>
2025-09-02 11:42:40 +0100

    d

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <div className="flex flex-col gap-5 py-7 px-7 md:py-11 md:px-[7rem]">
-      <h1 className="font-bold text-3xl text-center md:text-4xl">Welcome to the National Examinations Council Website</h1>
+      <h1 className="font-bold text-3xl text-center md:text-6xl">Welcome to the National Examinations Council Website</h1>
       <p>
         The National Examinations Council (NECO) is pleased to announce that the
         registration for the 2025 Senior School Certificate Examination
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -40,9 +40,9 @@ const Navbar = () => {
       </button>
       {menuIsOpen && (
         <div className="absolute top-0 left-0 flex justify-between  w-full h-full text-[14px] bg-red-500  text-white md:hidden">
-         <div className=" w-[35%] h-full bg-green-300 text-[30px] flex justify-end px-3 py-15">
-           <CgCloseR onClick={() => setMenuIsOpen(false)}/>
-         </div>
+          <div className=" w-[35%] h-full bg-gray-500 text-[30px] flex justify-end px-3 py-15">
+            <CgCloseR onClick={() => setMenuIsOpen(false)} />
+          </div>
 
           <div className="flex flex-col gap-10 w-[65%] bg-white px-3 pt-[3.5rem] text-black text-[25px] ">
             <h1>Menu</h1>
@@ -52,6 +52,9 @@ const Navbar = () => {
                   <a href={nav.href}>{nav.label}</a>
                 </li>
               ))}
+              <button className="bg-[#2DC866] text-white py-4 px-5 rounded-[5px] hidden md:flex">
+                Login
+              </button>
             </ul>
           </div>
         </div>
