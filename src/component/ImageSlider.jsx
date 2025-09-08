340b579d186df1a200c527e2d34f2b50bc92f4a6
Mubarak <adebowaleraji0103@gmail.com>
2025-09-08 16:11:26 +0100

    d

--- src/component/ImageSlider.jsx
+++ src/component/ImageSlider.jsx
@@ -4,16 +4,7 @@ import { Autoplay } from "swiper/modules";
 function ImageSlider() {
     return(
         <div>
-           <Swiper 
-            modules={[Autoplay]}
-        spaceBetween={2}
-        slidesPerView={1}
-        loop={true}
-        autoplay={{ delay: 2000, disableOnInteraction: false }}
-         breakpoints={{
-    610: {
-      slidesPerView: 2, // md: and above
-    }/>
+       <>
 
         </div>
     )
