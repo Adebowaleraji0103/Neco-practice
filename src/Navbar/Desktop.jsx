9014efd46d5642cd74a24ac3e3dab20cea2eef91
Mubarak <adebowaleraji0103@gmail.com>
2025-09-12 21:15:46 +0100

    f

--- src/Navbar/Desktop.jsx
+++ src/Navbar/Desktop.jsx
@@ -1,5 +1,23 @@
 const Desktop = () => {
-    return (  );
-}
- 
+  return (
+    <div>
+      <div className="flex items-center">
+        <h1 className="text-2xl">Logo</h1>
+
+        <ul className="relative gap-10 hidden flex-1 md:flex md:justify-center">
+          {Navlink.map((nav, index) => (
+            <li key={index}>
+              <a className="hover:text-gray-400" href={nav.href}>
+                {nav.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <Button />
+      </div>
+    </div>
+  );
+};
+
 export default Desktop;
