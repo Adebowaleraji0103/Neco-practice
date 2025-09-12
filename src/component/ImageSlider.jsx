f36a4c0151ca971edab0ddfd14bec16ee57d8965
Mubarak <adebowaleraji0103@gmail.com>
2025-09-12 20:02:37 +0100

    df

--- src/component/ImageSlider.jsx
+++ src/component/ImageSlider.jsx
@@ -5,17 +5,30 @@ function ImageSlider() {
   return (
     <div className="">
       <Swiper
+        modules={[Navigation]}
         spaceBetween={50}
-        slidesPerView={1}
-        onSlideChange={() => console.log("slide change")}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
         onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
       >
-        <SwiperSlide><img src="slider (1).png" alt="slider" /></SwiperSlide>
-        <SwiperSlide><img src="slider (1).png" alt="slider" /></SwiperSlide>
-        <SwiperSlide><img src="slider (1).png" alt="slider" /></SwiperSlide>
-        <SwiperSlide><img src="slider (1).png" alt="slider" /></SwiperSlide>
-        <SwiperSlide><img src="slider (1).png" alt="slider" /></SwiperSlide>
-        
+        <SwiperSlide>
+          <img src="slider (1).png" alt="slider" className="w-full" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="slider (1).png" alt="slider" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="slider (1).png" alt="slider" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="slider (1).png" alt="slider" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="slider (1).png" alt="slider" />
+        </SwiperSlide>
         ...
       </Swiper>
     </div>
