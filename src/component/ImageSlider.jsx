ade1eb5bbc6048b998b4d569ec73ecef515aa43d
Mubarak <adebowaleraji0103@gmail.com>
2025-09-12 19:54:23 +0100

    d

--- src/component/ImageSlider.jsx
+++ src/component/ImageSlider.jsx
@@ -2,19 +2,23 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 
 function ImageSlider() {
-  return <div>
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>slider (1).png</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
-  </div>;
+  return (
+    <div>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide><img src="slider (1).png" alt="slider" /></SwiperSlide>
+        <SwiperSlide><img src="slider (1).png" alt="slider" /></SwiperSlide>
+        <SwiperSlide><img src="slider (1).png" alt="slider" /></SwiperSlide>
+        <SwiperSlide><img src="slider (1).png" alt="slider" /></SwiperSlide>
+        <SwiperSlide><img src="slider (1).png" alt="slider" /></SwiperSlide>
+        
+        ...
+      </Swiper>
+    </div>
+  );
 }
 export default ImageSlider;
