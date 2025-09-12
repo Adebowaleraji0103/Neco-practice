bf19c7c059a6deaa7377ac085dc5177d5c43f265
Mubarak <adebowaleraji0103@gmail.com>
2025-09-12 21:46:01 +0100

    f

--- src/component/ImageSlider.jsx
+++ src/component/ImageSlider.jsx
@@ -1,47 +1,30 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 
-const Images = [
-  {
-    images: "slider (1).png",
-    images: "slider (2).png",
-    images: "slider (3).png",
-    images: "slider (4).png",
-    images: "slider (5).png",
-  },
-];
-
 function ImageSlider() {
   return (
     <div className="">
-      <SwiperSlide
+      <Swiper
         modules={[Autoplay]}
         spaceBetween={2}
         slidesPerView={1}
         loop={true}
         autoplay={{ delay: 2000, disableOnInteraction: false }}
       >
-        {/* <SwiperSlide className="w-full">
+        <SwiperSlide className="w-full">
           <img src="slider (1).png" alt="slider" className="w-full" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="slider (2).png" alt="slider" />
+          <img src="slider (2).png" alt="slider" className="w-full" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="slider (3).png" alt="slider" />
+          <img src="slider (3).png" alt="slider" className="w-full"/>
         </SwiperSlide>
         <SwiperSlide>
-          <img src="slider (4).png" alt="slider" />
+          <img src="slider (4).png" alt="slider" className="w-full"/>
         </SwiperSlide>
         <SwiperSlide>
-          <img src="slider (5).png" alt="slider" />
-        </SwiperSlide> */}
-        <SwiperSlide>
-          {Images.map((Images, index)=>(
-            <div key={index}>
-              <img src={Images} alt="" />
-            </div>
-          ))}
+          <img src="slider (5).png" alt="slider" className="w-full"/>
         </SwiperSlide>
       </Swiper>
     </div>
