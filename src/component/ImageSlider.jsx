0332e938f2d1d40efb04b7d91ae1c99f70425575
Mubarak <adebowaleraji0103@gmail.com>
2025-09-12 19:49:59 +0100

    f

--- src/component/ImageSlider.jsx
+++ src/component/ImageSlider.jsx
@@ -2,8 +2,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 
 function ImageSlider() {
-    return(
-  
-    )
+  return <div></div>;
 }
 export default ImageSlider;
