01ea2e06bf4beb0343b6244757a90edea0a40867
Mubarak <adebowaleraji0103@gmail.com>
2025-09-12 19:52:50 +0100

    f

--- src/component/ImageSlider.jsx
+++ src/component/ImageSlider.jsx
@@ -9,7 +9,7 @@ function ImageSlider() {
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>slider (1).png</SwiperSlide>
       <SwiperSlide>Slide 2</SwiperSlide>
       <SwiperSlide>Slide 3</SwiperSlide>
       <SwiperSlide>Slide 4</SwiperSlide>
