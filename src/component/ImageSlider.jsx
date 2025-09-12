659d5f00321506be5cbf76b6183596dd89962f23
Mubarak <adebowaleraji0103@gmail.com>
2025-09-12 20:10:08 +0100

    d

--- src/component/ImageSlider.jsx
+++ src/component/ImageSlider.jsx
@@ -5,25 +5,26 @@ function ImageSlider() {
   return (
     <div className="">
       <Swiper
-        modules={[Navigation]}
-        spaceBetween={50}
-        slidesPerView={3}
-        navigation
+        modules={[Autoplay]}
+        spaceBetween={2}
+        slidesPerView={1}
+        loop={true}
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
       >
         <SwiperSlide>
           <img src="slider (1).png" alt="slider" className="w-full" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="slider (1).png" alt="slider" />
+          <img src="slider (2).png" alt="slider" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="slider (1).png" alt="slider" />
+          <img src="slider (3).png" alt="slider" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="slider (1).png" alt="slider" />
+          <img src="slider (4).png" alt="slider" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="slider (1).png" alt="slider" />
+          <img src="slider (5).png" alt="slider" />
         </SwiperSlide>
         ...
       </Swiper>
