5bb8261120ce9609386015f89794c9be732f69b6
Mubarak <adebowaleraji0103@gmail.com>
2025-09-12 19:54:59 +0100

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./component/Hero";
+import ImageSlider from "./component/ImageSlider";
 import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Hero/>
+      <Hero />
+      <ImageSlider />
     </div>
   );
 }
