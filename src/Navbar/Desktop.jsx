8218258b56ebefe481d22cc8028eab2e782feb7b
Mubarak <adebowaleraji0103@gmail.com>
2025-09-12 21:26:52 +0100

    f

--- src/Navbar/Desktop.jsx
+++ src/Navbar/Desktop.jsx
@@ -11,22 +11,20 @@ const Navlink = [
 
 const Desktop = () => {
   return (
-    <div>
-      <div className="flex items-center">
-        <h1 className="text-2xl">Logo</h1>
+    <div className="flex justify-between items-center w-full">
+      <h1 className="text-2xl">Logo</h1>
 
-        <ul className="relative gap-10 hidden flex-1 md:flex md:justify-center">
-          {Navlink.map((nav, index) => (
-            <li key={index}>
-              <a className="hover:text-gray-400" href={nav.href}>
-                {nav.label}
-              </a>
-            </li>
-          ))}
-        </ul>
+      <ul className="relative gap-10 hidden flex-1 md:flex md:justify-center">
+        {Navlink.map((nav, index) => (
+          <li key={index}>
+            <a className="hover:text-gray-400" href={nav.href}>
+              {nav.label}
+            </a>
+          </li>
+        ))}
+      </ul>
 
-        <Button />
-      </div>
+      <Button />
     </div>
   );
 };
