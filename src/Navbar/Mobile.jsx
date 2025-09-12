f1eaabe8f080cc43b6aebd94330907cad5f90f21
Mubarak <adebowaleraji0103@gmail.com>
2025-09-12 21:16:52 +0100

    d

---
+++ src/Navbar/Mobile.jsx
@@ -0,0 +1,33 @@
+const Mobile = () => {
+  return (
+    <div>
+      <MdMenu
+        onClick={() => setMenuIsOpen(!menuIsOpen)}
+        className="md:hidden text-3xl relative"
+      />
+      {menuIsOpen && (
+        <div className="absolute top-0 left-0 flex justify-between  w-full h-full text-[14px] bg-red-500  text-white md:hidden">
+          <div className=" w-[35%] h-full bg-[#2DC866] text-[30px] flex justify-end px-3 py-15">
+            <CgCloseR onClick={() => setMenuIsOpen(false)} />
+          </div>
+
+          <div className="flex flex-col gap-10 w-[65%] bg-white px-3 pt-[3.5rem] text-black text-[25px] ">
+            <h1>Menu</h1>
+            <ul className="flex flex-col gap-6 text-[15px] text-gray-500">
+              {Navlink.map((nav, index) => (
+                <li key={index}>
+                  <a href={nav.href}>{nav.label}</a>
+                </li>
+              ))}
+            </ul>
+            <button className="bg-[#2DC866] text-white text-[16px] w-fit py-1 px-2 rounded-[5px] flex">
+              Login
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Mobile;
