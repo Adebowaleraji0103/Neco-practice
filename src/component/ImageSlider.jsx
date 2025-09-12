9043ed4a479cdfe9f72dc4e629e7f3a10cc540a4
Mubarak <adebowaleraji0103@gmail.com>
2025-09-12 20:03:14 +0100

    f

--- src/component/ImageSlider.jsx
+++ src/component/ImageSlider.jsx
@@ -9,10 +9,6 @@ function ImageSlider() {
         spaceBetween={50}
         slidesPerView={3}
         navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}
       >
         <SwiperSlide>
           <img src="slider (1).png" alt="slider" className="w-full" />
