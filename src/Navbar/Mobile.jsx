23c484efafa3f9b3cdcd15a4edf2c38d159be07f
Mubarak <adebowaleraji0103@gmail.com>
2025-10-03 16:57:28 +0100

    d

--- src/Navbar/Mobile.jsx
+++ src/Navbar/Mobile.jsx
@@ -36,7 +36,7 @@ const Mobile = () => {
                 </li>
               ))}
             </ul>
-            <button className="bg-[#328538] text-white text-[16px] w-fit py-1 px-2 rounded-[5px] flex">
+            <button className="bg-[#2DC866] text-white text-[16px] w-fit py-1 px-2 rounded-[5px] flex">
               Login
             </button>
           </div>
