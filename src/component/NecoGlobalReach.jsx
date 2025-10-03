be38cf58819b627630ed518b8462a343ed3851c2
Mubarak <adebowaleraji0103@gmail.com>
2025-10-03 17:10:40 +0100

    f

--- src/component/NecoGlobalReach.jsx
+++ src/component/NecoGlobalReach.jsx
@@ -1,9 +1,13 @@
 const NecoGlobalReach = () => {
-    return ( 
-        <div className="bg-[#2DC866]">
-            <h2>Neco Global Reach</h2>
-        </div>
-     );
-}
- 
+  return (
+    <div className="bg-[#2DC866]">
+      <h2>Neco Global Reach</h2>
+      <span>
+        NECO conducts examinations for Nigerian International Schools in the
+        following foreign countries
+      </span>
+    </div>
+  );
+};
+
 export default NecoGlobalReach;
