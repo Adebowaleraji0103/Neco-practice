d2c8a5370b47ca4ab36f385fac45506680815905
Mubarak <adebowaleraji0103@gmail.com>
2025-10-03 19:39:45 +0100

    d

--- src/component/NecoGlobalReach.jsx
+++ src/component/NecoGlobalReach.jsx
@@ -22,7 +22,7 @@ const NecoGlobalReach = () => {
     // },
   ];
   return (
-    <div className="mb-90 bg-[#36893C] text-white flex flex-col gap-[2rem]">
+    <div className="mb-90 bg-[#36893C] text-white flex flex-col gap-[2rem] padding">
       <div className=" text-xl text-center flex flex-col gap-4 padding md:text-[27px] ">
         <h2 className="font-semibold text-[2.3rem] md:text-[3.5rem]">
           NECO Global Reach
@@ -33,8 +33,8 @@ const NecoGlobalReach = () => {
         </span>
       </div>
       {country.map((card, index) => (
-        <div className="flex px-[1rem] ">
-          <ul key={index} className="bg-[#599A5E] flex flex-col gap-5 w-full rounded-[10px] font-semibold text-center py-[4rem]">
+        <div className="flex  ">
+          <ul key={index} className="bg-[#599A5E] flex flex-col gap-5 w-full rounded-[10px] font-semibold text-center py-[6rem]">
             <li className="text-[3rem] text-black">{card.short}</li>
             <li className="text-[1.4rem] ">{card.name}</li>
           </ul>
