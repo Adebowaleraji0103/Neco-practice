1a2125e4e7eb3edbe87356f21531525eaaaffa03
Mubarak <adebowaleraji0103@gmail.com>
2025-10-03 19:32:25 +0100

    d

--- src/component/NecoGlobalReach.jsx
+++ src/component/NecoGlobalReach.jsx
@@ -4,25 +4,25 @@ const NecoGlobalReach = () => {
       short: "AB",
       name: "Benin Republic",
     },
-    {
-      short: "AB",
-      name: " Republic",
-    },
-    {
-      short: "AB",
-      name: "Benin ",
-    },
-    {
-      short: "AB",
-      name: "Benin Republic",
-    },
-    {
-      short: "AB",
-      name: " Republic",
-    },
+    // {
+    //   short: "AB",
+    //   name: " Republic",
+    // },
+    // {
+    //   short: "AB",
+    //   name: "Benin ",
+    // },
+    // {
+    //   short: "AB",
+    //   name: "Benin Republic",
+    // },
+    // {
+    //   short: "AB",
+    //   name: " Republic",
+    // },
   ];
   return (
-    <div className="mb-90 bg-[#36893C] text-white">
+    <div className="mb-90 bg-[#36893C] text-white flex flex-col gap-[2rem]">
       <div className=" text-xl text-center flex flex-col gap-4 padding md:text-[27px] ">
         <h2 className="font-semibold text-[2.3rem] md:text-[3.5rem]">
           NECO Global Reach
@@ -33,12 +33,13 @@ const NecoGlobalReach = () => {
         </span>
       </div>
       {country.map((card, index) => (
-        <div key={index} className="bg-[#599A5E] flex flex-col gap-11">
-          {card.short}
-          {card.name}
+        <div className="flex">
+          <ul key={index} className="bg-[#599A5E] flex flex-col gap-11">
+            <li>{card.short}</li>
+            <li>{card.name}</li>
+          </ul>
         </div>
       ))}
-      
     </div>
   );
 };
