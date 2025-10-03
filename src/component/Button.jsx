e9262ad8da2d905bd54692fcfc73ff458c81565e
Mubarak <adebowaleraji0103@gmail.com>
2025-10-03 16:53:59 +0100

    d

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({text}) => {
   return (
-    <button className="bg-gray-800 text-white py-4 px-5 rounded-[5px] hidden md:flex">
+    <button className="bg-[#328538] text-white py-4 px-5 rounded-[5px] hidden md:flex">
       Login
     </button>
   );
