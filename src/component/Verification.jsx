972605ea9b1769465bb4b6121c29ba76e2541713
Mubarak <adebowaleraji0103@gmail.com>
2025-10-04 20:59:48 +0100

    dd

--- src/component/Verification.jsx
+++ src/component/Verification.jsx
@@ -2,9 +2,9 @@ const Verification = () => {
   return (
     <div className="mb-90 flex flex-col gap-[2rem] text-center padding bg-[#054C0F] text-white font-semibold ">
       <h2 className="font-bold text-[28px] ">e-Verify Online Result Verification/Confirmation Solution</h2>
-      <div>
+      <div className="flex flex-col  gap-5">
         <span>This is an online Result Verification/Confirmation system designed to perform dual result verification purposes viz, the Instant Result Verification and Institutional Bulk Verification. It guarantees instant authentication of academic or basic information about prospective candidates for admission and employment into academic institutions and workplaces respectively.</span>
-        <button>Apply here</button>
+        <button className="bg-white text-[#054C0F] w-fit py-5 px-9 rounded-[6px]">Apply here</button>
       </div>
     </div>
   );
