1bec872a6fc81894ed4edb30302bcfbb46c3ebab
Mubarak <adebowaleraji0103@gmail.com>
2025-10-04 09:07:47 +0100

    d

--- src/component/NecoGlobalReach.jsx
+++ src/component/NecoGlobalReach.jsx
@@ -51,7 +51,7 @@ const NecoGlobalReach = () => {
       >
         {country.map((card, index) => (
           <SwiperSlide key={index} className="">
-            <div className="  bg-[#599A5E] flex flex-col gap-5 w-full rounded-[17px] font-semibold text-center py-[6rem] transform transition duration-300 hover:scale-85 hover:overflow-hidden origin-center  hover:rounded-[17px]"> 
+            <div className="  bg-[#599A5E] flex flex-col gap-5 w-full rounded-[17px] font-semibold text-center py-[6rem] transform transition duration-300 hover:scale-85 overflow-hidden "> 
               <div className="text-[3rem] text-black ">{card.short}</div>
               <div className="text-[1.4rem]">{card.name}</div>
             </div>
