f471aea30de4ee310ad98d4aff78a4468d658ed3
Mubarak <adebowaleraji0103@gmail.com>
2025-10-04 09:37:26 +0100

    d

--- src/component/NecoGlobalReach.jsx
+++ src/component/NecoGlobalReach.jsx
@@ -30,35 +30,45 @@ const NecoGlobalReach = () => {
         </span>
       </div>
 
-      <Swiper
-        modules={[Autoplay]}
-        grabCursor={true}
-        loop={true}
-        speed={1900}
-        spaceBetween={25}
-        autoplay={{ delay: 1800, disableOnInteraction: false }}
-        className="w-full mx-auto"
-        breakpoints={{
-          640: {
-            slidesPerView: 1,
-          },
-          768: {
-            slidesPerView: 2,
-          },
-          1024: {
-            slidesPerView: 5,
-          },
-        }}
-      >
-        {country.map((card, index) => (
-          <SwiperSlide key={index} className="">
-            <div className="  bg-[#4A9E52] flex flex-col gap-5 w-full rounded-[17px] font-semibold text-center py-[6rem] border transform transition duration-300 hover:scale-85 overflow-hidden hover:bg-[#599A5E] "> 
-              <div className="text-[3rem] text-black ">{card.short}</div>
-              <div className="text-[1.4rem]">{card.name}</div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className="flex flex-col gap-14">
+        <Swiper
+          modules={[Autoplay]}
+          grabCursor={true}
+          loop={true}
+          speed={1900}
+          spaceBetween={25}
+          autoplay={{ delay: 1800, disableOnInteraction: false }}
+          className="w-full mx-auto"
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 5,
+            },
+          }}
+        >
+          {country.map((card, index) => (
+            <SwiperSlide key={index} className="">
+              <div className="  bg-[#4A9E52] flex flex-col gap-5 w-full rounded-[17px] font-semibold text-center py-[6rem] border transform transition duration-300 hover:scale-85 overflow-hidden hover:bg-[#599A5E] ">
+                <div className="text-[3rem] text-black ">{card.short}</div>
+                <div className="text-[1.4rem]">{card.name}</div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className="bg-[#4A9E52] border rounded-[17px] py-[4rem]">
+          <h2>Expanding Educational Excellence:
+          <span>
+            NECO's commitment to quality education transcends borders, serving
+            Nigerian students across Africa and the Middle East with
+            internationally recognized certification standards.
+          </span></h2>
+        </div>
+      </div>
     </div>
   );
 };
