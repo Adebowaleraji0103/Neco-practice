17852f27d56808a9fd2e69782f9b4ee9d8e5586d
Mubarak <adebowaleraji0103@gmail.com>
2025-10-06 00:44:49 +0100

    d

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -3,12 +3,12 @@ import Mobile from "./Mobile";
 
 const Navbar = () => {
   return (
-    <div>
-      <header className="flex justify-between items-center py-4 px-6 font-semibold text-gray-700 shadow-lg md:px-[7rem] md:py-3">
+    // <div>
+      <header className="flex justify-between items-center py-4 px-6 w-full fixed font-semibold text-gray-700 shadow-lg md:px-[7rem] md:py-3 ">
         <Desktop />
         <Mobile />
       </header>
-    </div>
+    {/* </div> */}
   );
 };
 
