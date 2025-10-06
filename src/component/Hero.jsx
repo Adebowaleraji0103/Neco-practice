138613ec513bfff3436c256601ca7176fad744e3
Mubarak <adebowaleraji0103@gmail.com>
2025-10-06 12:43:25 +0100

    d

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="flex flex-col gap-5 py-7 px-7 md:py-11 md:px-[7rem] ">
+    <div className="flex flex-col gap-5 py-7 px-7 mt-[5rem] md:py-11 md:px-[7rem] ">
       <h1 className="font-bold text-3xl text-center md:text-6xl md:leading-16">
         Welcome to the National Examinations Council Website
       </h1>
