3f6c6ec18ee69482c7ec43a41f7f9a1300227478
Mubarak <adebowaleraji0103@gmail.com>
2025-10-06 19:43:31 +0100

    d

--- src/Navbar/Desktop.jsx
+++ src/Navbar/Desktop.jsx
@@ -12,7 +12,8 @@ const Navlink = [
 const Desktop = () => {
   return (
     <div className="flex justify-between items-center w-full">
-      <h1 className="text-2xl">Logo</h1>
+      {/* <h1 className="text-2xl">Logo</h1> */}
+      <img src="neco-logo.png" alt="neco-logo alt" className="w-[70px] md:w-[90px]"/>
 
       <ul className="relative gap-10 hidden flex-1 md:flex md:justify-center">
         {Navlink.map((nav, index) => (
