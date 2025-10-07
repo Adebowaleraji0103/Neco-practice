54682fd4b439ec824dfd184419931dd3cbd6b05f
Mubarak <adebowaleraji0103@gmail.com>
2025-10-07 07:00:06 +0100

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import Navbar from "./component/HomePage/Navbar/Navbar";
-import Footer from "./component/HomePage/Footer";
-import Hero from "./component/HomePage/Hero";
-import ImageSlider from "./component/HomePage/ImageSlider";
-import LatestNews from "./component/HomePage/LatestNews";
-import NecoGlobalReach from "./component/HomePage/NecoGlobalReach";
-import Verification from "./component/HomePage/Verification";
 import HomePage from "./component/HomePage/HomePage";
 
 function App() {
   return (
     <div>
-      <HomePage/>
-      {/* <Navbar /> */}
-      {/* <Hero /> */}
-      {/* <ImageSlider /> */}
-      {/* <NecoGlobalReach /> */}
-      {/* <Verification /> */}
-      {/* <LatestNews /> */}
-      {/* <Footer /> */}
+      <HomePage />
     </div>
   );
 }
