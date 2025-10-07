4db124c69edfa582748b558d05f2a9bf5bf574f0
Mubarak <adebowaleraji0103@gmail.com>
2025-10-07 06:57:33 +0100

    d

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <div>
       <HomePage/>
       {/* <Navbar /> */}
-      <Hero />
+      {/* <Hero /> */}
       <ImageSlider />
       <NecoGlobalReach />
       <Verification />
--- src/component/HomePage/HomePage.jsx
+++ src/component/HomePage/HomePage.jsx
@@ -1,9 +1,13 @@
+import Hero from "./Hero";
+import ImageSlider from "./ImageSlider";
 import Navbar from "./Navbar/Navbar";
 
 function HomePage() {
   return (
     <div>
       <Navbar />
+      <Hero/>
+      <ImageSlider/>
     </div>
   );
 }
