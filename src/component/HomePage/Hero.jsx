3c1ab5efc295ef05b905be7bb34e80681c6278d6
Mubarak <adebowaleraji0103@gmail.com>
2025-10-07 09:03:25 +0100

    f

--- src/component/HomePage/Hero.jsx
+++ src/component/HomePage/Hero.jsx
@@ -1,16 +1,12 @@
+import Note from "./Note";
+
 const Hero = () => {
   return (
     <div className="flex flex-col gap-5 py-7 px-7 mt-[5rem] md:py-11 md:px-[7rem] ">
       <h1 className="font-bold text-3xl text-center md:text-6xl md:leading-16">
         Welcome to the National Examinations Council Website
       </h1>
-      <p className="text-[#6b7280] text-[17px]">
-          This website is a personal project created for educational and
-          practice purposes. It is inspired by the design and content of 
-          <a href="https://neco.gov.ng/" className="text-blue-900 font-bold mx-2">neco.gov.ng</a> but is not affiliated with or endorsed by the
-          original creators. Some elements have been modified to suit the scope
-          of this project.
-      </p>
+      <Note/>
     </div>
   );
 };
