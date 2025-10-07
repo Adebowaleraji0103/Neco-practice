933728883187c291e086e08b08de48b42e72c63b
Mubarak <adebowaleraji0103@gmail.com>
2025-10-07 09:07:04 +0100

    f

---
+++ src/component/AboutPage/OurHistory.jsx
@@ -0,0 +1,12 @@
+const OurHsitory = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+      repellendus error minima fuga, laboriosam similique exercitationem earum
+      temporibus corrupti, rerum, nam reiciendis ipsam magnam aperiam sapiente
+      explicabo quidem nesciunt saepe!
+    </div>
+  );
+};
+
+export default OurHsitory;
