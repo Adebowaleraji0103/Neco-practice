c299124f9dbc6daf917a32164cbf6146a98fad10
Mubarak <adebowaleraji0103@gmail.com>
2025-10-08 11:57:10 +0100

    d

--- src/component/AboutPage/OurHistory.jsx
+++ src/component/AboutPage/OurHistory.jsx
@@ -5,29 +5,34 @@ const OurHsitory = () => {
         Our History
       </h1>
       <div className="flex justify-between flex-col-reverse lg:flex-row lg:gap-20 items-center ">
-        <p className="text-[#4E4E4E] mt-5 lg:mt-0 text-sm lg:text-base md:w-[60%] ">
-          The establishment of the National Examinations Council (NECO) in April
-          1999, is a culmination of reaction of the Federal Government to
-          widespread escalating agitations for a national examination outfit
-          that would be responsive to the Nigerian needs and demands as well as
-          cope with the annually increasing number of secondary school
-          candidates to take Senior School Certificate Examination (SSCE)
-          nationwide. Series of panels had earlier been set up to address the
-          issues before this vital decision was taken. The origin of the issues
-          dates back to 1977, during the Murtala/Obasanjo administration, when
-          there was a widespread leakage of West African School Certificate
-          Examination WASSCE questions. The public outcry that greeted the mass
-          leakage led the Federal Government to set up the Sogbetun commission
-          of enquiry with specific mandate to look into the remote causes of the
-          leakage and make appropriate recommendations to government. Key among
-          the findings of the committee was that the workload of WAEC was beyond
-          its capacity, hence, it was difficult for WAEC to effectively manage
-          the conduct of a hitch-free examination. The Sogbetun commission
-          recommended among other things, the establishment of additional
-          examination bodies to reduce the workload of WAEC and make it more
-          efficient. The recommendation of the committee was not implemented at
-          the time...
-        </p>
+        <div className="flex flex-col gap-5 text-[#4E4E4E] mt-5 lg:mt-0 text-sm font-semibold lg:text-base md:w-[60%]">
+          <p>
+            The establishment of the National Examinations Council (NECO) in
+            April 1999, is a culmination of reaction of the Federal Government
+            to widespread escalating agitations for a national examination
+            outfit that would be responsive to the Nigerian needs and demands as
+            well as cope with the annually increasing number of secondary school
+            candidates to take Senior School Certificate Examination (SSCE)
+            nationwide. Series of panels had earlier been set up to address the
+            issues before this vital decision was taken.
+          </p>
+          <p>
+            The origin of the issues dates back to 1977, during the
+            Murtala/Obasanjo administration, when there was a widespread leakage
+            of West African School Certificate Examination WASSCE questions. The
+            public outcry that greeted the mass leakage led the Federal
+            Government to set up the Sogbetun commission of enquiry with
+            specific mandate to look into the remote causes of the leakage and
+            make appropriate recommendations to government. Key among the
+            findings of the committee was that the workload of WAEC was beyond
+            its capacity, hence, it was difficult for WAEC to effectively manage
+            the conduct of a hitch-free examination. The Sogbetun commission
+            recommended among other things, the establishment of additional
+            examination bodies to reduce the workload of WAEC and make it more
+            efficient. The recommendation of the committee was not implemented
+            at the time...
+          </p>
+        </div>
         <img src="aboutpic.png" alt="" className="md:w-[30%]" />
       </div>
     </div>
