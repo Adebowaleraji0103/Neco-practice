6ab60a6c751f559f862a93e745d4bc8fdea08527
Mubarak <adebowaleraji0103@gmail.com>
2025-10-08 13:53:45 +0100

    d

--- src/component/AboutPage/NecoWorldWide.jsx
+++ src/component/AboutPage/NecoWorldWide.jsx
@@ -1,9 +1,27 @@
+const allCard = [
+  { name: "Ab", country: "Saudi" },
+  { name: "Ab", country: "Saudi" },
+  { name: "Ab", country: "Saudi" },
+  { name: "Ab", country: "Saudi" },
+  { name: "Ab", country: "Saudi" },
+  { name: "Ab", country: "Saudi" },
+  { name: "Ab", country: "Saudi" },
+  
+];
+
 const NecoWorldWide = () => {
-    return ( 
-        <div>
-            <h1>NECO'S WORLDWIDE REACH</h1>
-        </div>
-     );
-}
- 
-export default NecoWorldwide;
+  return (
+    <div>
+      <h1>NECO'S WORLDWIDE REACH</h1>
+      <p>
+        NECO is a world class examination body conducting standard assessment
+        and evaluation for Nigerian International schools in the following
+        Foreign Countries:
+      </p>
+
+      {}
+    </div>
+  );
+};
+
+export default NecoWorldWide;
