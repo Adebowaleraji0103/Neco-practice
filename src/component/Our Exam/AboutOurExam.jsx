4e01ad9458e9f5392228d4952da5c57a94cddd6c
Mubarak <adebowaleraji0103@gmail.com>
2025-10-27 15:43:58 +0100

    d

--- src/component/Our Exam/AboutOurExam.jsx
+++ src/component/Our Exam/AboutOurExam.jsx
@@ -1,13 +1,47 @@
-name : "Senior School Certificate Examination (SSCE)",
+const allCards = [
+  {
+    name: "Senior School Certificate Examination (SSCE)",
+    desc: "The Senior School Certificate Examination (SSCE) Internal, is the examination conducted for candidates at the end of their Secondary School Education and the SSCE External is conducted for candidates that have attempted and have deficiency(ies) to make-up.",
+    firstBtn: "Internal",
+    secondBtn: "External",
+  },
 
+  {
+    name: "Senior School Certificate Examination (SSCE)",
+    desc: "The Senior School Certificate Examination (SSCE) Internal, is the examination conducted for candidates at the end of their Secondary School Education and the SSCE External is conducted for candidates that have attempted and have deficiency(ies) to make-up.",
+    firstBtn: "Internal",
+    secondBtn: "External",
+  },
 
+  {
+    name: "Senior School Certificate Examination (SSCE)",
+    desc: "The Senior School Certificate Examination (SSCE) Internal, is the examination conducted for candidates at the end of their Secondary School Education and the SSCE External is conducted for candidates that have attempted and have deficiency(ies) to make-up.",
+    firstBtn: "Internal",
+    secondBtn: "External",
+  },
 
+  {
+    name: "Senior School Certificate Examination (SSCE)",
+    desc: "The Senior School Certificate Examination (SSCE) Internal, is the examination conducted for candidates at the end of their Secondary School Education and the SSCE External is conducted for candidates that have attempted and have deficiency(ies) to make-up.",
+    firstBtn: "Internal",
+    secondBtn: "External",
+  },
+
+  {
+    name: "Senior School Certificate Examination (SSCE)",
+    desc: "The Senior School Certificate Examination (SSCE) Internal, is the examination conducted for candidates at the end of their Secondary School Education and the SSCE External is conducted for candidates that have attempted and have deficiency(ies) to make-up.",
+    firstBtn: "Internal",
+    secondBtn: "External",
+  },
+];
 
 const AboutOurExam = () => {
   return (
     <div className=" py-7 px-7 mt-[5rem] md:py-11 md:px-[7rem] ">
       <div className="flex flex-col gap-[1.5rem] text-[#6b7280]  ">
-        <h1 className="text-[25px] font-bold text-black ">About Our Examinations</h1>
+        <h1 className="text-[25px] font-bold text-black ">
+          About Our Examinations
+        </h1>
         <p>
           One of the core mandates of the Council as espoused in the enabling
           Act is the conduct of the Senior School Certificate Examination for
