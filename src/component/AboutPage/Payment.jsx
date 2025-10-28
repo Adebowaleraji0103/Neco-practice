8cf776ef19af8e7ad9555416060b403ae2fceb9e
Mubarak <adebowaleraji0103@gmail.com>
2025-10-28 13:47:04 +0100

    vfj

--- src/component/AboutPage/Payment.jsx
+++ src/component/AboutPage/Payment.jsx
@@ -2,7 +2,7 @@ const Payment = () => {
   return (
     <div className="p-[10rem] bg-amber-600">
       <div>
-        <h1>Fees & Payments</h1>
+        <h1 >Fees & Payments</h1>
         <p>
           Payments for all items with payment-code should be made through the
           NECO payment platform on payments.neco.gov.ng
