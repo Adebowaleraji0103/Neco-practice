b1d4f9b2810ac5333f60dd135aed81d00f7a4e81
Mubarak <adebowaleraji0103@gmail.com>
2025-10-28 13:17:20 +0100

    d

--- src/component/AboutPage/Payment.jsx
+++ src/component/AboutPage/Payment.jsx
@@ -1,9 +1,15 @@
 const Payment = () => {
-    return (  
-        <div>
-            
-        </div>
-    );
-}
- 
+  return (
+    <div>
+      <div>
+        <h1>Fees & Payments</h1>
+        <p>
+          Payments for all items with payment-code should be made through the
+          NECO payment platform on payments.neco.gov.ng
+        </p>
+      </div>
+    </div>
+  );
+};
+
 export default Payment;
