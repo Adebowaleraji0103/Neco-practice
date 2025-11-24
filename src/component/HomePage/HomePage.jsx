d7fd88e2d8987f901d85dae33f9026330456e11f
Mubarak <adebowaleraji0103@gmail.com>
2025-11-24 07:10:51 +0100

    whxh

--- src/component/HomePage/HomePage.jsx
+++ src/component/HomePage/HomePage.jsx
@@ -2,14 +2,12 @@ import Footer from "./Footer";
 import Hero from "./Hero";
 import ImageSlider from "./ImageSlider";
 import LatestNews from "./LatestNews";
-// import Navbar from "./Navbar/Navbar";
 import NecoGlobalReach from "./NecoGlobalReach";
 import Verification from "./Verification";
 
 function HomePage() {
   return (
     <div>
-      {/* <Navbar /> */}
       <Hero />
       <ImageSlider />
       <NecoGlobalReach />
