b0c07509698305bea21a01a0c47d91008336c763
Mubarak <adebowaleraji0103@gmail.com>
2025-11-24 06:49:50 +0100

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutPage from "./component/AboutPage/AboutPage";
 import HomePage from "./component/HomePage/HomePage";
-import Navbar from "./component/HomePage/Navbar/Navbar";
+// import Navbar from "./component/Navbar/Navbar";
 import Footer from "./component/HomePage/Footer";
 import AboutOurExam from "./component/Our Exam/AboutOurExam";
 import Payment from "./component/AboutPage/Payment";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Navbar/>
+        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
-          <Route path="/exam" element={<AboutOurExam/>}/>
-          <Route path="/payment" element={<Payment/>}/>
+          <Route path="/exam" element={<AboutOurExam />} />
+          <Route path="/payment" element={<Payment />} />
         </Routes>
-      <Footer/>
+        <Footer />
       </BrowserRouter>
     </div>
   );
