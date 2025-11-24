7fd7f843aae45055ce2ca66c7f82d1a83842ba3a
Mubarak <adebowaleraji0103@gmail.com>
2025-11-24 07:21:26 +0100

    d

--- src/component/Our Exam/AboutOurExam.jsx
+++ src/component/Our Exam/AboutOurExam.jsx
@@ -1,29 +1,3 @@
-const allCards = [
-  {
-    name: "Senior School Certificate Examination (SSCE)",
-    desc: "The Senior School Certificate Examination (SSCE) Internal, is the examination conducted for candidates at the end of their Secondary School Education and the SSCE External is conducted for candidates that have attempted and have deficiency(ies) to make-up.",
-    firstBtn: { title: "Internal", color: "#ddd" },
-    secondBtn: "External",
-    color: "bg-cyan-100",
-    buttonColor: "bg-cyan-700",
-  },
-  {
-    name: "Junior School Certificate Examination (SSCE)",
-    desc: "The Senior School Certificate Examination (SSCE) Internal, is the examination conducted for candidates at the end of their Secondary School Education and the SSCE External is conducted for candidates that have attempted and have deficiency(ies) to make-up.",
-    firstBtn: { title: "Internal", color: "#ddd" },
-    // secondBtn: "External",
-    color: "bg-cyan-100",
-    buttonColor: "bg-cyan-700",
-  },
-  {
-    name: "Senior School Certificate Examination (SSCE)",
-    desc: "The Senior School Certificate Examination (SSCE) Internal, is the examination conducted for candidates at the end of their Secondary School Education and the SSCE External is conducted for candidates that have attempted and have deficiency(ies) to make-up.",
-    firstBtn: { title: "Internal", color: "#ddd" },
-    secondBtn: "External",
-    bg: "#4444",
-    buttonColor: "bg-black",
-  },
-];
 
 const AboutOurExam = () => {
   return (
---
+++ src/component/Our Exam/examProductCard.js
@@ -0,0 +1,26 @@
+const allCards = [
+  {
+    name: "Senior School Certificate Examination (SSCE)",
+    desc: "The Senior School Certificate Examination (SSCE) Internal, is the examination conducted for candidates at the end of their Secondary School Education and the SSCE External is conducted for candidates that have attempted and have deficiency(ies) to make-up.",
+    firstBtn: { title: "Internal", color: "#ddd" },
+    secondBtn: "External",
+    color: "bg-cyan-100",
+    buttonColor: "bg-cyan-700",
+  },
+  {
+    name: "Junior School Certificate Examination (SSCE)",
+    desc: "The Senior School Certificate Examination (SSCE) Internal, is the examination conducted for candidates at the end of their Secondary School Education and the SSCE External is conducted for candidates that have attempted and have deficiency(ies) to make-up.",
+    firstBtn: { title: "Internal", color: "#ddd" },
+    // secondBtn: "External",
+    color: "bg-cyan-100",
+    buttonColor: "bg-cyan-700",
+  },
+  {
+    name: "Senior School Certificate Examination (SSCE)",
+    desc: "The Senior School Certificate Examination (SSCE) Internal, is the examination conducted for candidates at the end of their Secondary School Education and the SSCE External is conducted for candidates that have attempted and have deficiency(ies) to make-up.",
+    firstBtn: { title: "Internal", color: "#ddd" },
+    secondBtn: "External",
+    bg: "#4444",
+    buttonColor: "bg-black",
+  },
+];
